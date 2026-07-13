1a296c41b45a0fc9ef4b2e6f81deb4743740490b
Gemsly Hoshino <gemslyho@outlook.com>
2026-07-13 12:41:11 +0800

    chore: align Hugo config with new domain and theme

--- build&deploy.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-call hugo 
-:: 调用Cloudflare wrangler部署命令（-p指定项目名，对应原脚本的blog-home-pub）
-call wrangler pages deploy ./public --project-name blog-home-pub
-:: call edgeone pages deploy ./public -n blog-home-pub
-
-pause
-
--- debug.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-start http://127.0.0.1:1313
-call hugo server -D
-pause
